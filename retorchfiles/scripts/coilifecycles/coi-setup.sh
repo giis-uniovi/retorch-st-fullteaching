76c3bf97375a40a3539fcb388b2ead8bc63c1ba6
Augusto <augustocristian@uniovi.es>
2024-12-31 18:00:26 +0100

    First draft with the new orchestrator version
    Scripts generated using the tool, passing in CI but needs some improvements in order to solve the problems

--- retorchfiles/scripts/coilifecycles/coi-setup.sh
+++ retorchfiles/scripts/coilifecycles/coi-setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# The coi-setup.sh script provides all the necessary commands to set up the infrastructure before executing
+# the TJobs. It performs debugging actions, such as outputting the kernel, library, and Docker versions.
+# Additionally, it prunes old videos and execution data, and verifies that the required Selenium containers are running.
+
+if [ "$#" -ne 0 ]; then
+    "$SCRIPTS_FOLDER/printLog.sh" "ERROR" "COI-set-up" "Usage: $0 - This script does not take any parameters"
+    exit 1
+fi
 
 SEL_VIDEO_DIR="/opt/selenoid/video/"
 SEL_LOG_DIR="/opt/selenoid/logs/"
@@ -7,10 +15,10 @@ SEL_LOG_DIR="/opt/selenoid/logs/"
 remove_old_files() {
     dir=$1
     days=$2
-    echo "Removing older files ($days days) in $dir! The number of files prior to remove:"
+    "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "COI-set-up" "Removing older files ($days days) in $dir! The number of files prior to remove:"
     find "$dir" | wc -l
     find "$dir" -mindepth 1 -maxdepth 1 -mtime +$days -exec rm -rf {} \;
-    echo "Removing older files ($days days) in $dir! The number of files after remove:"
+    "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "COI-set-up" "Removing older files ($days days) in $dir! The number of files after remove:"
     find "$dir" | wc -l
 }
 
@@ -20,6 +28,15 @@ OUTPUTDIRCOI="$WORKSPACE/retorchcostestimationdata/exec$BUILD_NUMBER/COI.data"
 # Record start time for COI setup
 COISETUPSTART=$(date +%s%3N)
 
+# Prompt some system information
+docker_version=$(docker version)
+docker_compose_version=$(docker compose version | awk '{print $4}')
+curl_version=$(curl --version | head -n 1 | awk '{print $2}')
+kernel_version=$(uname -r)
+os_version=$(cat /etc/os-release | grep PRETTY_NAME | cut -d '"' -f 2)
+
+"$SCRIPTS_FOLDER/printLog.sh" "INFO" "COI-set-up" "Os version : $os_version, Kernel version: $kernel_version,  Docker-compose version: $docker_compose_version, curl-version: $curl_version"
+"$SCRIPTS_FOLDER/printLog.sh" "INFO" "COI-set-up" "Full Docker versions:\n $docker_version"
 # Remove older videos (older than 15 days)
 remove_old_files "$SEL_VIDEO_DIR" 15
 
@@ -31,21 +48,40 @@ mkdir -p "$WORKSPACE/retorchcostestimationdata/exec$BUILD_NUMBER"
 mkdir -p "$WORKSPACE/artifacts"
 mkdir -p "$SUT_LOCATION/tmp"
 
-# Pull Docker images
-"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "COI-set-up" "Checking that the Selenium hub and node are present"
-IMAGES=("selenium/hub" "selenium/node-chrome")
+# START Custom COI commands
+"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Start executing custom commands"
+echo "This COI-setup dont have any kind of specific commands"
+"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "End executing custom commands"
+# END Custom COI commands
+
+
+# Check if the provided string appears in any running container's name
+check_container_running() {
+    local search_term="$1"
+
+    # We capture the ID of any running container that matches the name string
+    local container_id=$(docker ps --filter "name=$search_term" --filter "status=running" -q)
+
+    if [ -n "$container_id" ]; then
+        "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "COI-set-up" "Found running container containing '$search_term' in the name."
+    else
+        "$SCRIPTS_FOLDER/printLog.sh" "ERROR" "COI-set-up" "No running container name matches '$search_term'. Aborting."
+        exit 1
+    fi
+}
+
+check_container_running "selenium-hub"
+check_container_running "chrome-video"
+check_container_running "chrome-node"
 
 echo "Building images of SUT"
 
 cd "$SUT_LOCATION"
 # Use double quotes for Docker Compose files TEMPORAL FIX
-docker compose -f "docker-compose.yml" --env-file "$WORKSPACE/retorchfiles/envfiles/tjobc.env" --ansi never build
-
-# Check the exit status of the last command
-if [ $? -eq 0 ]; then
-    echo "Images for SUT"
+if docker compose -f "docker-compose.yml" --env-file "$WORKSPACE/retorchfiles/envfiles/tjoba.env" --ansi never build; then
+    "$SCRIPTS_FOLDER/printLog.sh" "INFO" "COI-set-up" "Images for the SUT created successfully"
 else
-    echo "Failed to build images of SUT"
+    "$SCRIPTS_FOLDER/printLog.sh" "ERROR" "COI-set-up" "Failed to build images of SUT"
     exit 1
 fi
 
--- retorchfiles/scripts/tjoblifecycles/tjob-teardown.sh
+++ retorchfiles/scripts/tjoblifecycles/tjob-teardown.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
+# The tjob-teardown.sh script provides all the necessary commands to tear-down each TJob's Resources after the
+# test execution has ended. It stores the container logs of the current TJob, tear down the containers
+# and execute the custom commands provided in the custom-tjob-teardown file.
+
 set -e
+
+if [ "$#" -ne 2 ]; then
+    "$SCRIPTS_FOLDER/printLog.sh" "ERROR" "TJob-tear-down" "Usage: $0 <TJobName> <Stage>"
+    exit 1
+fi
 
 # Execute the script to write timestamp
 "$SCRIPTS_FOLDER/writetime.sh" "$2" "$1"
-
+"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-tear-down" "Starting the TJob tear-down"
 # Store docker logs
-DIRECTORY_PATH="$WORKSPACE/target/containerlogs/$1"
-
-if [ ! -d "$DIRECTORY_PATH" ]; then
-  echo "Directory for storing logs doesnt exist creating..."
-  mkdir -p $DIRECTORY_PATH
-fi
-
-for CONTAINER_NAME in $(docker ps -a --format "{{.Names}}" --filter Name=$1); do
-  echo "Storing lof con container $CONTAINER_NAME in TJob $1"
-  docker logs $CONTAINER_NAME &>"$DIRECTORY_PATH/$CONTAINER_NAME.log"
-done
+"$SCRIPTS_FOLDER/storeContainerLogs.sh" "$1"
 
 # Change to SUT location
 cd "$SUT_LOCATION"
 
 # Tear down Docker containers and volumes
-echo "Tearing down Docker containers and volumes for TJOB $1"
+"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-tear-down" "Tearing down Docker containers and volumes for TJOB $1"
 docker compose -f docker-compose.yml --env-file "$WORKSPACE/retorchfiles/envfiles/$1.env" --ansi never -p "$1" down --volumes
 
 # Return to the original working directory
 cd "$WORKSPACE"
 
+# START Custom Set-up commands
+"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Start executing custom commands"
+echo "This TJOB-teardown dont have any kind of specific commands"
+"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "End executing custom commands"
+# END Custom Set-up commands
 
+"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-tear-down" "Tear-down ended"
 # Execute the script to write timestamp again
 "$SCRIPTS_FOLDER/writetime.sh" "$2" "$1"
