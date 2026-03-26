48646e1422af2c92d868d4eb0cecaa564698955a
Augusto <augustocristian@uniovi.es>
2026-03-26 15:06:53 +0100

    Moving the fhiles to the new directory

--- .retorch/scripts/tjoblifecycles/tjob-setup.sh
+++ .retorch/scripts/tjoblifecycles/tjob-setup.sh
@@ -21,7 +21,7 @@ echo "This TJOB-setup dont have any kind of specific commands"
 # Deploy containers
 cd "$SUT_LOCATION"
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Deploying containers for TJOB $1"
-docker compose -f docker-compose.yml --env-file "$WORKSPACE/retorchfiles/envfiles/$1.env" --ansi never -p "$1" up -d
+docker compose -f docker-compose.yml --env-file "$WORKSPACE/.retorch/envfiles/$1.env" --ansi never -p "$1" up -d
 
 if [ $? -ne 0 ]; then
     "$SCRIPTS_FOLDER/printLog.sh" "ERROR" "$1-set-up" "Docker compose failed,writing end time of the set-up"
