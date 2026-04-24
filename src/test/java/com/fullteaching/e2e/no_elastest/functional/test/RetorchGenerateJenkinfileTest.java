0a5f5af39ecced388463daaaba0b6588017ecfcb
Augusto <augustocristian@uniovi.es>
2026-04-24 20:04:59 +0200

    Starting to fix the plotter

--- src/test/java/com/fullteaching/e2e/no_elastest/functional/test/RetorchGenerateJenkinfileTest.java
+++ src/test/java/com/fullteaching/e2e/no_elastest/functional/test/RetorchGenerateJenkinfileTest.java
@@ -4,9 +4,13 @@ package com.fullteaching.e2e.no_elastest.functional.test;
 
 import giis.retorch.orchestration.classifier.EmptyInputException;
 import giis.retorch.orchestration.generator.OrchestrationGenerator;
+import giis.retorch.orchestration.model.ExecutionPlan;
 import giis.retorch.orchestration.orchestrator.NoFinalActivitiesException;
 import giis.retorch.orchestration.scheduler.NoTGroupsInTheSchedulerException;
 import giis.retorch.orchestration.scheduler.NotValidSystemException;
+import giis.retorch.profiling.main.UsageProfilerToolBox;
+import giis.retorch.profiling.profilegeneration.ProfileGenerator;
+import giis.retorch.profiling.profilegeneration.ProfilePlotter;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +23,15 @@ class RetorchGenerateJenkinfileTest {
     void testGenerateJenkinsfile() throws NoFinalActivitiesException, NoTGroupsInTheSchedulerException, EmptyInputException, IOException, URISyntaxException, NotValidSystemException, ClassNotFoundException {
         OrchestrationGenerator orch= new OrchestrationGenerator();
         orch.generateJenkinsfile("com.fullteaching.e2e.no_elastest.functional.test","FullTeaching", "./");
+
+        UsageProfilerToolBox usageProfiler = new UsageProfilerToolBox();
+        usageProfiler.generateAverageDurationCSVFile("executiondata","./averagedurationfile.csv");
+        ExecutionPlan plan=orch.getExecutionPlan("com.fullteaching.e2e.no_elastest.functional.test","FullTeaching");
+
+        ProfileGenerator profileGenerator = new ProfileGenerator();
+        profileGenerator.generateExecutionPlanCapacitiesUsage(plan,"./averagedurationfile.csv","./output/profile.csv",3600,1);
+        ProfilePlotter plotter = new ProfilePlotter("./output/profile.csv");
+        plotter.generateTotalTJobUsageProfileCharts("./output","RETORCH","VM");
     }
 }
 
